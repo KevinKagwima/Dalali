b22c51d5d1c38f555900d31d0ac24cce774fe964
kevokagwima <kevokagwima@gmail.com>
2026-06-11 16:46:31 +0300

    updated files

--- app.py
+++ app.py
@@ -14,48 +14,47 @@ from Payments.routes import payments
 from Errors.handlers import errors
 from Utils.filters import average_filter
 
-def create_app():
-  app = Flask(__name__)
-  app.config.from_object(Config)
-  app.jinja_env.filters["average"] = average_filter
-  db.init_app(app)
-  Migrate(app, db)
-  login_manager = LoginManager()
-  login_manager.init_app(app)
-  cache.init_app(app)
-  celery_init_app(app)
+# def create_app():
+app = Flask(__name__)
+app.config.from_object(Config)
+app.jinja_env.filters["average"] = average_filter
+db.init_app(app)
+Migrate(app, db)
+login_manager = LoginManager()
+login_manager.init_app(app)
+cache.init_app(app)
+celery_init_app(app)
 
-  app.register_blueprint(auth)
-  app.register_blueprint(dalali)
-  app.register_blueprint(client)
-  app.register_blueprint(main)
-  app.register_blueprint(payments)
-  app.register_blueprint(admin)
-  app.register_blueprint(errors)
-  app.register_blueprint(crm)
+app.register_blueprint(auth)
+app.register_blueprint(dalali)
+app.register_blueprint(client)
+app.register_blueprint(main)
+app.register_blueprint(payments)
+app.register_blueprint(admin)
+app.register_blueprint(errors)
+app.register_blueprint(crm)
 
-  login_manager.blueprint_login_views = {
-    'dalali': '/auth/signin',
-    'client': '/auth/signup',
-    'admin': '/auth/signup',
-    'crm': '/auth/signup',
-  }
-  login_manager.login_message_category = "danger"
-  login_manager.refresh_view = '/auth/signin'
-  login_manager.needs_refresh_message = "Your previous session timed out. Login again"
-  login_manager.needs_refresh_message_category = "info"
+login_manager.blueprint_login_views = {
+  'dalali': '/auth/signin',
+  'client': '/auth/signup',
+  'admin': '/auth/signup',
+  'crm': '/auth/signup',
+}
+login_manager.login_message_category = "danger"
+login_manager.refresh_view = '/auth/signin'
+login_manager.needs_refresh_message = "Your previous session timed out. Login again"
+login_manager.needs_refresh_message_category = "info"
 
-  @login_manager.user_loader
-  def load_user(user_id):
-    try:
-      return Users.query.filter_by(unique_id=user_id).first()
-    except Exception as e:
-      flash(f"Error loading user: {str(e)}", "danger")
+@login_manager.user_loader
+def load_user(user_id):
+  try:
+    return Users.query.filter_by(unique_id=user_id).first()
+  except Exception as e:
+    flash(f"Error loading user: {str(e)}", "danger")
 
-  return app
+# return app
 
-app = create_app()
+# app = create_app()
 
 if __name__ == "__main__":
-  app = create_app()
   app.run(debug=True)
